1b37de088111f7333fe8bf6c800ca6db5d8428c9
Rahul Kumar <rahulkumar703@outlook.com>
2023-12-21 01:40:55 +0530

    added admin link

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,151 +1,125 @@
 "use client"
-import { useEffect } from 'react';
+import useScroll from '@/hooks/useScroll';
+import { useEffect, useRef } from 'react';
 
-import Button from '@/components/button/Button';
-import BottomGlitter from '@/components/StyledText/BottomGlitter';
-import styles from '@/styles/events.module.css';
-import AnimatedScrollButton from '@/components/AnimatedScrollButton';
-import EventsData from '@/lib/data/EventsData';
-import Image from 'next/image';
 
-function Events() {
-  useEffect(() => {
-    const handleClipping = () => {
-      if (document.documentElement.clientWidth > 900) {
-        const allElements = [
-          ...document.querySelectorAll(`.${styles.contentWrapper}`),
-        ];
-        allElements.forEach((element) => {
-          const circle =
-            element.previousSibling.previousSibling.firstChild.firstChild
-              .firstChild;
-          const currentImageBox = element.previousSibling;
-          const boundingBox = element.getBoundingClientRect();
-          const clippedBoxHeight = boundingBox.height;
-          const windowHeight = document.documentElement.clientHeight;
-          const distanceFromTop = boundingBox.top + clippedBoxHeight / 13;
-          let percentageOfElementVisible;
-          let opacityRelativeToPosition;
-
-          if (distanceFromTop > windowHeight) {
-            percentageOfElementVisible = 0;
-            opacityRelativeToPosition = 0;
-          } else if (distanceFromTop > 0) {
-            percentageOfElementVisible =
-              (windowHeight - distanceFromTop) / clippedBoxHeight;
-            // Setting the upper limit of percentage to 1
-            percentageOfElementVisible = Math.min(
-              percentageOfElementVisible,
-              1,
-            );
-            opacityRelativeToPosition = 1;
-          } else {
-            percentageOfElementVisible = 1;
-          }
-          if (
-            element.parentNode.previousSibling &&
-            percentageOfElementVisible > 0
-          ) {
-            const previousImageBox =
-              element.parentNode.previousSibling.childNodes[1];
-            previousImageBox.style.opacity = Math.max(
-              1 - percentageOfElementVisible * 1.5,
-              0,
-            );
-          }
-          currentImageBox.style.opacity = opacityRelativeToPosition;
-          circle.setAttribute('r', percentageOfElementVisible * 1.5);
-        });
-      }
-    };
-    document.addEventListener('scroll', handleClipping);
-    return () => document.removeEventListener('scroll', handleClipping);
-  }, []);
+export default function Events() {
 
   return (
-    <>
-      <div>
-        <div className="text-center mt-10 mb-8 sm:my-8">
-          <div className="flex flex-col justify-between h-[calc(100vh-100px-3rem)] items-center sm:h-[calc(90vh-100px-1rem)]">
-            <BottomGlitter text="Our Events" />
-            <AnimatedScrollButton
-              onClick={() =>
-                document.getElementById('scrolled-to').scrollIntoView()
-              }
-            />
-          </div>
+    <div className='container-70'>
+      <section className={`flex xl:flex-col flex-auto my-40 sm:my-20`} id='create'>
+        <div className={`mt-0 sm:mt-10 pr-11 box-border w-2/5 xl:w-full`}>
+          <h2 className={`sticky top-36 text-6xl sm:text-5xl font-semibold`}>Events</h2>
         </div>
-
-        <div className={styles.wrapper} id="scrolled-to">
-          {EventsData.map(({ title, description, bgImage, link }, index) => (
-            <EventBox
-              title={title}
-              description={description}
-              src={bgImage}
-              link={link}
-              id={`event-${index}`}
-              key={title}
-            />
-          ))}
+        <div className={`text-2xl mb-10 sm:mb-7 xl:mt-16 sm:text-lg mt-0 sm:mt-10 pl-11 box-border w-3/5 xl:w-full xl:pl-0`}>
+          Zero One Coding Club hosts fun events like workshops, hackathons, and contests. These help us learn and have a good time. We get to improve our coding skills, be creative in hackathons, and enjoy friendly contests. It&apos;s a cool place for both beginners and coding fans!
         </div>
-      </div>
-    </>
-  );
-}
-
-function EventBox({ title, description, src, link, id }) {
-  return (
-    <div className="contents lg:grid lg:gap-8 lg:justify-items-center">
-      <svg width="0" height="0">
-        <defs>
-          <clipPath id={id} clipPathUnits="objectBoundingBox">
-            <circle
-              cx="0.5"
-              cy="1.2"
-              r="0"
-              transform="translate(0.5 1.2) scale(0.8806584362139918 1) translate(-0.5 -1.2)"
-            />
-          </clipPath>
-        </defs>
-      </svg>
-
-      <div className={styles.banner}>
-        <div
-          className="relative w-full h-full"
-          style={{ clipPath: `url(#${id})` }}
-        >
-          <div className="absolute top-0 left-0 right-0 bottom-0 z-10 lg:static">
-            <Image
-              width={100}
-              height={100}
-              src={src}
-              alt="abc"
-              className="h-full w-full object-cover object-top"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.contentWrapper}>
-        <div className={styles.content}>
-          <h2 className="text-5xl font-bold sm:text-3xl">{title}</h2>
-          <p className="text-xl leading-normal sm:text-lg sm:font-light">
-            {description}
-          </p>
-          <a
-            href={link}
-            target="_blank"
-            className="flex w-[fit-content] rounded-full"
-            rel="noreferrer"
-          >
-            <Button>
-              <span className="z-50 block">View More</span>
-            </Button>
-          </a>
-        </div>
+      </section>
+      <div className="flex flex-col gap-10">
+        <OnGoingEvent />
+        <Upcoming />
+        <Past />
       </div>
     </div>
   );
 }
 
-export default Events;
+
+const OnGoingEvent = () => {
+  const ref = useRef();
+  useScroll(ref);
+
+
+  return (
+    <section ref={ref} className={`flex xl:flex-col flex-auto mt-40 sm:mt-20 fadeonscroll`} id='create'>
+      <div className={`mt-0 sm:mt-10 pr-11 box-border w-2/5 xl:w-full`}>
+        <h2 className={`sticky top-36 text-6xl sm:text-5xl font-semibold`}>Ongoing</h2>
+      </div>
+
+      <div className="text-2xl mb-10 sm:mb-7 xl:mt-16 sm:text-lg mt-0 sm:mt-10 pl-11 box-border w-3/5 xl:w-full xl:pl-0">
+        <div className=" border-t-2 border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2 ">
+          <h2 className=' text-accent font-semibold text-4xl sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>00:34:00</h2>
+        </div>
+      </div>
+    </section >
+
+  )
+}
+
+
+const Upcoming = () => {
+  const ref = useRef();
+
+  useScroll(ref);
+  return (
+    <section ref={ref} className={`flex xl:flex-col flex-auto mt-40 sm:mt-20 fadeonscroll`} id='create'>
+      <div className={`mt-0 sm:mt-10 pr-11 box-border w-2/5 xl:w-full`}>
+        <h2 className={`sticky top-36 text-6xl sm:text-5xl font-semibold`}>Upcoming</h2>
+      </div>
+
+      <div className="text-2xl mb-10 sm:mb-7 xl:mt-16 sm:text-lg mt-0 sm:mt-10 pl-11 box-border w-3/5 xl:w-full xl:pl-0">
+        <div className=" border-t-2 border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2 ">
+          <h2 className=' text-accent font-semibold text-4xl sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>17th Jan 2023 <span>4:30 PM</span></h2>
+        </div>
+        <div className="border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2  pt-0">
+          <h2 className=' text-accent font-semibold text-4xl  sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>17th Jan 2023 <span>4:30 PM</span></h2>
+        </div>
+        <div className="border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2  pt-0">
+          <h2 className=' text-accent font-semibold text-4xl  sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>17th Jan 2023 <span>4:30 PM</span></h2>
+        </div>
+        <div className="border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2  pt-0">
+          <h2 className=' text-accent font-semibold text-4xl  sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>17th Jan 2023 <span>4:30 PM</span></h2>
+        </div>
+        <div className="border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2  pt-0">
+          <h2 className=' text-accent font-semibold text-4xl  sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>17th Jan 2023 <span>4:30 PM</span></h2>
+        </div>
+      </div>
+    </section>
+
+  )
+}
+
+
+const Past = () => {
+  const ref = useRef();
+
+  useScroll(ref);
+
+  return (
+    <section ref={ref} className={`flex xl:flex-col flex-auto my-40 sm:my-20 fadeonscroll`} id='create'>
+      <div className={`mt-0 sm:mt-10 pr-11 box-border w-2/5 xl:w-full`}>
+        <h2 className={`sticky top-36 text-6xl sm:text-5xl font-semibold`}>Past</h2>
+      </div>
+
+      <div className="text-2xl mb-10 sm:mb-7 xl:mt-16 sm:text-lg mt-0 sm:mt-10 pl-11 box-border w-3/5 xl:w-full xl:pl-0">
+        <div className=" border-t-2 border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2 ">
+          <h2 className=' text-accent font-semibold text-4xl sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>17th Jan 2023</h2>
+        </div>
+        <div className="border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2  pt-0">
+          <h2 className=' text-accent font-semibold text-4xl  sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>17th Jan 2023</h2>
+        </div>
+        <div className="border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2  pt-0">
+          <h2 className=' text-accent font-semibold text-4xl  sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>17th Jan 2023</h2>
+        </div>
+        <div className="border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2  pt-0">
+          <h2 className=' text-accent font-semibold text-4xl  sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>17th Jan 2023</h2>
+        </div>
+        <div className="border-b-2 border-dashed mt-3 flex flex-1 justify-between items-center p-4 w-full gap-6 sm:gap-2  pt-0">
+          <h2 className=' text-accent font-semibold text-4xl  sm:text-xl'>Coder69</h2>
+          <h2 className='flex gap-6 sm:gap-2'>17th Jan 2023</h2>
+        </div>
+      </div>
+    </section>
+
+  )
+}
---
+++ src/hooks/useScroll.js
@@ -0,0 +1,23 @@
+"use client"
+import { useEffect } from "react";
+const useScroll = (ref) => {
+    return (
+        useEffect(() => {
+            const handleScroll = () => {
+                if (ref.current) {
+                    if (window.innerHeight + 100 > ref.current.getBoundingClientRect().y) {
+                        ref.current.classList.add('active');
+                    } else {
+                        ref.current.classList.remove('active');
+                    }
+                }
+            };
+
+            window.addEventListener('scroll', handleScroll);
+
+            return () => window.removeEventListener('scroll', handleScroll);
+        }, [])
+    )
+}
+
+export default useScroll
