305175bb7b1046d09e682463cba8ec116e9e0e4d
Rahul Kumar <rahulkimar703@outlook.com>
2024-03-10 13:08:34 +0530

    minor bug fixes

--- src/models/Questions.js
+++ src/models/Questions.js
@@ -13,21 +13,21 @@ const QuestionSchema = new mongoose.Schema(
             trim: true,
             required: [true, 'Problem Statement is Required.']
         },
-        inputFormat: {
-            type: String,
-            trim: true,
-            required: [true, 'Input Format is Required.']
-        },
-        outputFormat: {
-            type: String,
-            trim: true,
-            required: [true, 'Output Format is Required.']
-        },
-        constraints: {
-            type: String,
-            trim: true,
-            required: [true, 'Constraints is Required.']
-        },
+        // inputFormat: {
+        //     type: String,
+        //     trim: true,
+        //     required: [true, 'Input Format is Required.']
+        // },
+        // outputFormat: {
+        //     type: String,
+        //     trim: true,
+        //     required: [true, 'Output Format is Required.']
+        // },
+        // constraints: {
+        //     type: String,
+        //     trim: true,
+        //     required: [true, 'Constraints is Required.']
+        // },
         point: {
             type: Number,
             default: 0,
@@ -40,23 +40,29 @@ const QuestionSchema = new mongoose.Schema(
             required: [true, "Difficulty level is required"]
 
         },
-        testCases: [
-            {
-                input: {
-                    type: String,
-                    trim: true,
-                },
-                output: {
-                    type: String,
-                    trim: true,
-                    required: [true, 'Output is Required for the given testcase.']
-                },
-                isPublic: {
-                    type: Boolean,
-                    default: false
-                }
-            }
-        ]
+        link: {
+            type: String,
+            lowercase: true,
+            trim: true,
+            required: [true, "Problem Link is Required"]
+        }
+        // testCases: [
+        //     {
+        //         input: {
+        //             type: String,
+        //             trim: true,
+        //         },
+        //         output: {
+        //             type: String,
+        //             trim: true,
+        //             required: [true, 'Output is Required for the given testcase.']
+        //         },
+        //         isPublic: {
+        //             type: Boolean,
+        //             default: false
+        //         }
+        //     }
+        // ]
 
     }, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
 
