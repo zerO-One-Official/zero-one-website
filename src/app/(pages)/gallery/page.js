5f906f5f82b161a242eda05ec45bb99a42a7f133
Rahul Kumar <rahulkumar703@outlook.com>
2024-07-10 12:51:40 +0530

    fixed gallery

--- src/app/(pages)/gallery/page.js
+++ src/app/(pages)/gallery/page.js
@@ -2,6 +2,7 @@ import { getGallery } from "@/action/gallery";
 import AnimatedScrollButton from "@/components/AnimatedScrollButton";
 import BottomGlitter from "@/components/StyledText/BottomGlitter";
 import { GalleryImages } from "@/components/gallery/gallery";
+import Skeleton from "@/components/skeleton/skeleton";
 
 
 const GalleryPage = async () => {
@@ -20,16 +21,19 @@ const GalleryPage = async () => {
 
   return (
     <>
-      <div className="mt-10 mb-8 sm:my-8">
-        <div className="flex flex-col justify-between h-[calc(100vh-100px-3rem)] items-center sm:h-[calc(90vh-100px-1rem)]">
+
+
+
+      <div className="mt-10 mb-8 sm:my-8 px-20 2xl:px-10 xl:px-8 sm:px-6 xs:px-3">
+        <div className="flex flex-col justify-between items-center">
           <BottomGlitter text="Our Gallery" />
-          <AnimatedScrollButton scrollTo="scrolled-to" />
+          {/* <AnimatedScrollButton scrollTo="scrolled-to" /> */}
         </div>
         <div id="scrolled-to" className="min-h-screen pt-10">
           {
             groupedGal.map((group, index) => {
               return <div key={index} className="flex flex-col">
-                <h2 className="pl-10 font-semibold text-2xl">{group[0].eventName}</h2>
+                <h2 className="font-semibold text-2xl">{group[0].eventName}</h2>
                 <GalleryImages gallery={JSON.stringify(group)} />
               </div>
             })
