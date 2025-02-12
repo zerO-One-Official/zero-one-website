50e93b37c616f39fd9f618e2f611195c5bbf2e5b
Rahul <RahulKumar703@outlook.com>
2025-02-12 10:53:54 +0530

    solved uploadthings error

--- src/app/(pages)/user/[username]/page.js
+++ src/app/(pages)/user/[username]/page.js
@@ -15,180 +15,195 @@ import FilledCertificate from "@/components/certificates/FilledCertificate";
 import { getUserEvents } from "@/action/event";
 
 function capitalizeFirstChar(str) {
-    if (str.length === 0) return str; // Handle empty string case
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  if (str.length === 0) return str; // Handle empty string case
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 export async function generateMetadata({ params }) {
-    // read route params
-    const username = params.username;
-    const user = await getUser(username);
-    const name = user ? `${capitalizeFirstChar(user.firstName)} ${capitalizeFirstChar(user.lastName)}` : 'User not Found'
+  // read route params
+  const username = params.username;
+  const user = await getUser(username);
+  const name = user
+    ? `${capitalizeFirstChar(user.firstName)} ${capitalizeFirstChar(
+        user.lastName
+      )}`
+    : "User not Found";
 
-    return {
-        title: name,
-    };
+  return {
+    title: name,
+  };
 }
 
 const UserPage = async ({ params }) => {
+  const session = await getServerSession(options);
 
+  const loggedInUser = session?.user.username;
 
+  const username = params.username;
 
-    const session = await getServerSession(options)
+  const user = await getUser(username);
 
-    const loggedInUser = session?.user.username;
+  const userEvents = await getUserEvents(user?._id);
 
+  const certificates = await getUserCertificates(user?._id);
 
+  return user ? (
+    <div className="container-70 flex flex-col gap-4 min-h-[calc(100vh-88px)] pt-16">
+      <section className="flex flex-col items-center gap-6 border border-l-white/5 border-t-white/5 border-r-black/25 border-b-black/25 shadow-cus  p-6 rounded-3xl relative">
+        <div className="flex w-full md:gap-6 gap-10 items-center sm:flex-col">
+          <div className="p-2 border-4 md:border-2 border-accent rounded-full shrink-0">
+            <Image
+              src={user?.profilePic}
+              width={160}
+              height={160}
+              alt={user?.firstName}
+              className="md:w-20 md:h-20 lg:w-32 lg:h-32 w-36 h-36 object-cover rounded-full shadow"
+            />
+          </div>
+          <div className="p-4 ">
+            <h1 className="capitalize text-4xl md:text-3xl font-semibold sm:text-center">
+              {user.firstName} {user.lastName}
+            </h1>
+            <p className="text-zinc-500 capitalize text-xl sm:text-lg sm:text-center font-semibold">
+              {user.branch}
+            </p>
+            <p className="text-zinc-500 text-lg font-bold flex items-center sm:justify-center">
+              <MdAlternateEmail className="fill-white/40 md:w-4 md:h-4 w-6 h-6 mr-1" />
+              {user.username}
+            </p>
 
-    const username = params.username
+            <p className="capitalize text-accent text-lg font-bold flex items-center sm:justify-center">
+              {user.position}
+            </p>
+          </div>
+          {username === loggedInUser ? (
+            <Link
+              href={`/user/${loggedInUser}/edit`}
+              className="block rounded-full absolute top-4 right-4 p-4 shadow-btn border border-l-white/5 border-t-white/5 border-r-black/25 border-b-black/25 "
+            >
+              <BiEdit className="w-4 h-4" />
+            </Link>
+          ) : null}
+        </div>
+        <div className="flex gap-6 xl:flex-col w-full">
+          {user?.bio ? (
+            <>
+              <div className="flex-1 p-4 space-y-2">
+                <p className="flex items-baseline font-semibold">
+                  <DiCodeigniter className="w-4 h-4 mr-2" />
+                  Bio
+                </p>
+                <p className="text-zinc-500 flex items-center" title={user.bio}>
+                  {user.bio.slice(0, 100)}
+                  {user.bio.length > 100 ? "..." : ""}
+                </p>
+              </div>
 
-    const user = await getUser(username);
+              <div className="xl:w-full xl:h-[2px] w-[2px] h-inherit bg-white/10 rounded-md" />
+            </>
+          ) : null}
 
-    const userEvents = await getUserEvents(user?._id);
+          <div className="flex-1 p-4 space-y-2">
+            <p className="text-zinc-500 flex items-center">
+              <PiGenderIntersexBold className="stroke-ztext-zinc-500 md:w-4 md:h-4 w-6 h-6 mr-2" />
+              {user.gender}
+            </p>
+            <p className="text-zinc-500 flex items-center">
+              <IoSchool className="stroke-ztext-zinc-500 md:w-4 md:h-4 w-6 h-6 mr-2" />
+              {user.roll}
+            </p>
+          </div>
 
-    const certificates = await getUserCertificates(user?._id);
+          <div className="xl:w-full xl:h-[2px] w-[2px] h-inherit bg-white/10 rounded-md" />
 
-
-    return (
-        user ?
-            <div className="container-70 flex flex-col gap-4 min-h-[calc(100vh-88px)] pt-16">
-
-
-                <section className="flex flex-col items-center gap-6 border border-l-white/5 border-t-white/5 border-r-black/25 border-b-black/25 shadow-cus  p-6 rounded-3xl relative">
-                    <div className="flex w-full md:gap-6 gap-10 items-center sm:flex-col">
-                        <div className="p-2 border-4 md:border-2 border-accent rounded-full shrink-0">
-                            <Image src={user?.profilePic} width={160} height={160} alt={user?.firstName} className="md:w-20 md:h-20 lg:w-32 lg:h-32 w-36 h-36 object-cover rounded-full shadow" />
-                        </div>
-                        <div className="p-4 ">
-                            <h1 className="capitalize text-4xl md:text-3xl font-semibold sm:text-center">
-                                {user.firstName} {user.lastName}
-
-                            </h1>
-                            <p className="text-zinc-500 capitalize text-xl sm:text-lg sm:text-center font-semibold">{user.branch}</p>
-                            <p className="text-zinc-500 text-lg font-bold flex items-center sm:justify-center">
-                                <MdAlternateEmail className="fill-white/40 md:w-4 md:h-4 w-6 h-6 mr-1" />
-                                {user.username}
-                            </p>
-
-                            <p className="capitalize text-accent text-lg font-bold flex items-center sm:justify-center">{user.position}</p>
-                        </div>
-                        {
-                            username === loggedInUser ?
-
-                                <Link href={`/user/${loggedInUser}/edit`} className="block rounded-full absolute top-4 right-4 p-4 shadow-btn border border-l-white/5 border-t-white/5 border-r-black/25 border-b-black/25 "
-                                >
-                                    <BiEdit className="w-4 h-4" />
-                                </Link>
-                                :
-                                null
-                        }
-                    </div>
-                    <div className="flex gap-6 xl:flex-col w-full">
-                        {
-                            user?.bio ?
-                                <>
-                                    <div className="flex-1 p-4 space-y-2">
-                                        <p className="flex items-baseline font-semibold">
-                                            <DiCodeigniter className="w-4 h-4 mr-2" />
-                                            Bio
-                                        </p>
-                                        <p className="text-zinc-500 flex items-center" title={user.bio}>
-                                            {user.bio.slice(0, 100)}
-                                            {user.bio.length > 100 ? '...' : ''}
-                                        </p>
-                                    </div>
-
-                                    <div className="xl:w-full xl:h-[2px] w-[2px] h-inherit bg-white/10 rounded-md" />
-                                </>
-                                :
-                                null
-                        }
-
-                        <div className="flex-1 p-4 space-y-2">
-                            <p className="text-zinc-500 flex items-center">
-                                <PiGenderIntersexBold className='stroke-ztext-zinc-500 md:w-4 md:h-4 w-6 h-6 mr-2' />
-                                {user.gender}
-                            </p>
-                            <p className="text-zinc-500 flex items-center">
-                                <IoSchool className='stroke-ztext-zinc-500 md:w-4 md:h-4 w-6 h-6 mr-2' />
-                                {user.roll}
-                            </p>
-                        </div>
-
-                        <div className="xl:w-full xl:h-[2px] w-[2px] h-inherit bg-white/10 rounded-md" />
-
-                        <div className="flex-1 p-4 space-y-2">
-                            <div className="flex items-center">
-                                <HiEnvelope className='stroke-ztext-zinc-500  md:w-4 md:h-4 w-6 h-6 mr-2 shrink-0' />
-                                <a href={`mailto:${user.email}`} className="text-zinc-500  overflow-hidden text-ellipsis">{user.email}</a>
-                            </div>
-                            {user.gitHub ?
-                                <div className="flex items-center">
-                                    <IoLogoGithub className='stroke-ztext-zinc-500  md:w-4 md:h-4 w-6 h-6 mr-2 shrink-0' />
-                                    <a href={user.gitHub} className="text-zinc-500 overflow-hidden text-ellipsis">{user.gitHub}</a>
-                                </div>
-                                :
-                                null
-                            }
-                            {user.linkedIn ?
-                                <div className="flex items-center">
-                                    <BiLogoLinkedinSquare className='stroke-ztext-zinc-500  md:w-4 md:h-4 w-6 h-6 mr-2 shrink-0' />
-                                    <a href={user.linkedIn} className="text-zinc-500 overflow-hidden text-ellipsis">{user.linkedIn}</a>
-                                </div>
-                                :
-                                null
-                            }
-                        </div>
-                    </div>
-                </section>
-                <div className="flex gap-5 xl:gap-2 xl:flex-col flex-row" >
-                    <section className="mt-2 space-y-2 flex-1">
-                        {
-                            certificates.length > 0 ?
-                                <>
-                                    <BottomGlitter text={'Certificates'} className={'max-w-fit'} />
-                                    <div className="gap-2 grid grid-cols-[repeat(auto-fit,minmax(230px,240px))]">
-                                        {certificates.map((certificate, index) => (
-                                            <Link href={`/certificate?cn=${certificate.certificateNumber}`} className="flex flex-col card p-4" key={index} >
-                                                <FilledCertificate certificate={JSON.stringify(certificate)} />
-                                                <h3 className="text-lg pt-2 font-semibold ">{certificate.template.eventName}</h3>
-                                            </Link>
-                                        ))
-                                        }
-                                    </div>
-                                </>
-                                :
-                                null
-                        }
-                    </section>
-                    <section className="mt-2 space-y-2 xl:ml-0 ml-auto">
-                        {
-                            userEvents.length > 0 ? <>
-                                <BottomGlitter text={'Activities'} className={'max-w-fit'} />
-                                <div className="gap-2 grid grid-cols-1 xl:grid-cols-[repeat(auto-fill,minmax(230px,1fr))]">
-                                    {
-                                        userEvents.map((event, index) => (
-                                            <Link href={`/events/${event.name}`} className="card flex items-center justify-center h-16" key={index} >
-                                                <h3 className="text-lg pt-2 font-semibold ">{event.name}</h3>
-                                            </Link>
-                                        ))
-                                    }
-                                </div>
-                            </>
-                                :
-                                null
-                        }
-                    </section>
-
-                </div>
+          <div className="flex-1 p-4 space-y-2">
+            <div className="flex items-center">
+              <HiEnvelope className="stroke-ztext-zinc-500  md:w-4 md:h-4 w-6 h-6 mr-2 shrink-0" />
+              <a
+                href={`mailto:${user.email}`}
+                className="text-zinc-500  overflow-hidden text-ellipsis"
+              >
+                {user.email}
+              </a>
             </div>
-            :
-            <section className="container-70">
-                <div className="mt-16 flex flex-col items-center gap-6 border border-white/5 shadow-cus  p-6 rounded-3xl relative">
-                    <h2 className="text-xl">User Not Found</h2>
-                </div>
-            </section>
-    )
-}
+            {user.gitHub ? (
+              <div className="flex items-center">
+                <IoLogoGithub className="stroke-ztext-zinc-500  md:w-4 md:h-4 w-6 h-6 mr-2 shrink-0" />
+                <a
+                  href={user.gitHub}
+                  className="text-zinc-500 overflow-hidden text-ellipsis"
+                >
+                  {user.gitHub}
+                </a>
+              </div>
+            ) : null}
+            {user.linkedIn ? (
+              <div className="flex items-center">
+                <BiLogoLinkedinSquare className="stroke-ztext-zinc-500  md:w-4 md:h-4 w-6 h-6 mr-2 shrink-0" />
+                <a
+                  href={user.linkedIn}
+                  className="text-zinc-500 overflow-hidden text-ellipsis"
+                >
+                  {user.linkedIn}
+                </a>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </section>
+      <div className="flex gap-5 xl:gap-2 xl:flex-col flex-row">
+        <section className="mt-2 space-y-2 flex-1">
+          {certificates.length > 0 ? (
+            <>
+              <BottomGlitter text={"Certificates"} className={"max-w-fit"} />
+              <div className="gap-2 grid grid-cols-[repeat(auto-fit,minmax(230px,240px))]">
+                {certificates.map((certificate, index) => (
+                  <Link
+                    href={`/certificate?cn=${certificate.certificateNumber}`}
+                    className="flex flex-col card p-4"
+                    key={index}
+                  >
+                    <FilledCertificate
+                      certificate={JSON.stringify(certificate)}
+                    />
+                    <h3 className="text-lg pt-2 font-semibold ">
+                      {certificate.template.eventName}
+                    </h3>
+                  </Link>
+                ))}
+              </div>
+            </>
+          ) : null}
+        </section>
+        <section className="mt-2 space-y-2 xl:ml-0 ml-auto">
+          {userEvents.length > 0 ? (
+            <>
+              <BottomGlitter text={"Activities"} className={"max-w-fit"} />
+              <div className="gap-2 grid grid-cols-1 xl:grid-cols-[repeat(auto-fill,minmax(230px,1fr))]">
+                {userEvents.map((event, index) => (
+                  <Link
+                    href={`/events/${event.name}`}
+                    className="card flex items-center justify-center h-16"
+                    key={index}
+                  >
+                    <h3 className="text-lg pt-2 font-semibold ">
+                      {event.name}
+                    </h3>
+                  </Link>
+                ))}
+              </div>
+            </>
+          ) : null}
+        </section>
+      </div>
+    </div>
+  ) : (
+    <section className="container-70">
+      <div className="mt-16 flex flex-col items-center gap-6 border border-white/5 shadow-cus  p-6 rounded-3xl relative">
+        <h2 className="text-xl">User Not Found</h2>
+      </div>
+    </section>
+  );
+};
 
-export default UserPage
+export default UserPage;
--- src/app/api/uploadthing/core.js
+++ src/app/api/uploadthing/core.js
@@ -1,23 +1,23 @@
 import { getServerSession } from "next-auth";
 import { createUploadthing } from "uploadthing/next";
 import { options } from "../auth/[...nextauth]/options";
+import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-
 export const ourFileRouter = {
-    profilePicUploader: f({ image: { maxFileSize: "1MB", maxFileCount: 1 } })
-        .middleware(async ({ req }) => {
-            const session = await getServerSession(options);
-            const id = session?.user?._id;
-            if (!session || !id) return null;
-            return { userId: id }
-        })
-        .onUploadComplete(async ({ metadata, file }) => {
-            console.log("Upload complete for userId:", metadata.userId);
+  profilePicUploader: f({ image: { maxFileSize: "1MB", maxFileCount: 1 } })
+    .middleware(async ({ req }) => {
+      const session = await getServerSession(options);
+      const id = session?.user?._id;
+      if (!session || !id) throw new UploadThingError("Unauthorized");
+      return { userId: id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      console.log("Upload complete for userId:", metadata.userId);
 
-            console.log("file url", file.url);
+      console.log("file url", file.url);
 
-            return { uploadedBy: metadata.userId };
-        }),
+      return { uploadedBy: metadata.userId };
+    }),
 };
